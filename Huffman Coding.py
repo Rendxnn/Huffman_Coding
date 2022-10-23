b4610b772308fab022cc6a048ab3392ba04eb6a8
Rendxnn <88605945+Rendxnn@users.noreply.github.com>
2022-10-23 01:05:34 -0500

    Update Huffman Coding.py

--- Huffman Coding.py
+++ Huffman Coding.py
@@ -1,144 +1,110 @@
-from collections import deque
-
-
-class Node:
-    def __init__(self, char, freq):
-        self.char = char
-        self.freq = freq
-        self.left = None
-        self.right = None
-
-
-def readFrequency(message):
+def readFrequencies(message):
     frequencies = {}
     for char in message:
-        if char in frequencies:
-            frequencies[char] += 1
-        else:
-            frequencies[char] = 1
+        frequencies[char] = frequencies[char] + 1 if char in frequencies else 1
     return frequencies
 
 
-def create(frequencies):
-    nodes = []
-    for char in frequencies:
-        nodes.append(Node(char, frequencies[char]))
-    nodes.sort(key=lambda x: x.freq)
-    return nodes
-
-
-def buildTree(nodes):
-    if len(nodes) == 1:
-        return nodes[0]
-    new = Node(None, nodes[0].freq + nodes[1].freq)
-    new.left = nodes[0]
-    new.right = nodes[1]
-    nodes = [new] + nodes[2:]
-    nodes.sort(key=lambda x: x.freq)
-    return buildTree(nodes)
-
-
-def assignCodes(root, code='', codes=None):
-    if not codes:
-        codes = {}
-    if root:
-        if not root.left and not root.right:
-            codes[root.char] = code
-        codes = assignCodes(root.left, code + '0', codes)
-        codes = assignCodes(root.right, code + '1', codes)
-        return codes
-    return codes
-
-
-def writeCode(message, codes):
-    newMessage = ''
-    for char in message:
-        newMessage += codes[char]
-    return newMessage
-
-
-def decode(coded, root):
-    current = root
-    message = ''
-    for char in coded:
-        if current.char:
-            message += current.char
-            current = root
-        if char == '0':
-            current = current.left
-        elif char == '1':
-            current = current.right
-    message += current.char
-    return message
-
-
-def bfs(root):
-    stack = deque([[root]])
-    ans = []
-    while stack:
-        temp = []
-        level = stack.popleft()
-        if not level:
-            break
-        for node in level:
-            if node.left:
-                temp.append(node.left)
-            if node.right:
-                temp.append(node.right)
-        stack.append(temp)
-        ans.append([x.char for x in temp])
-    return ans
+def calculateCode(frequencies):
+    t = [pair for pair in frequencies.items()]
+    while len(t) > 1:
+        m1, m2 = float('inf'), float('inf')
+        char1, char2 = '', ''
+        for frequency in t:
+            if frequency[1] < m1:
+                m2, m1 = m1, frequency[1]
+                char2, char1 = char1, frequency[0]
+            elif frequency[1] < m2:
+                m2 = frequency[1]
+                char2 = frequency[0]
+        t = [((char1, char2), m1 + m2)] + [char for char in t if char[0] != char1 and char[0] != char2]
+    return t
 
 
 def calculateLength(frequencies):
     n = len(frequencies)
-    i = 0
     array = [0] * (n * 2)
+    i = 0
     for char in frequencies:
         array[n + i] = frequencies[char]
         array[i] = n + i
         i += 1
-    h = n
-    array = siftMinHeap(array)
-    newArr = []
-    for x in range(len(array) // 2):
-        newArr.append(array[array[x]])
-    # print(newArr)
+    h = n - 1
+    array = buildMinHeap(array)
+    while h > 0:
+        m1 = array[0]
+        array[0] = array[h]
+        h = h - 1
+        sift(array, h)
+        m2 = array[0]
+        array[h + 1] = array[m1] + array[m2]
+        array[0] = h + 1
+        array[m1] = h + 1
+        array[m2] = h + 1
+        sift(array, h)
+    array[1] = 0
+    for i in range(2, 2 * n):
+        array[i] = array[array[i]] + 1
+    i = 0
+    lengths = []
+    for char in frequencies:
+        lengths.append(array[n + i])
+        # print(char, array[n + i])
+        i += 1
+    # print(array)
+    return lengths
 
 
-def siftMinHeap(array):
+def buildMinHeap(array):
     n = len(array)
     while True:
-        # print(array)
         mod = 0
         for i in range(n // 2):
             if i * 2 + 1 < n // 2:
                 if array[array[i]] > array[array[i * 2 + 1]]:
-                    array[array[i]], array[array[i * 2 + 1]] = array[array[i * 2 + 1]], array[array[i]]
+                    array[i], array[i * 2 + 1] = array[i * 2 + 1], array[i]
                     mod += 1
                     break
             if i * 2 + 2 < n // 2:
                 if array[array[i]] > array[array[i * 2 + 2]]:
-                    array[array[i]], array[array[i * 2 + 2]] = array[array[i * 2 + 2]], array[array[i]]
+                    array[i], array[i * 2 + 2] = array[i * 2 + 2], array[i]
                     mod += 1
                     break
         if mod == 0:
             return array
 
 
+def sift(array, n):
+    parent = 0
+    minimum = 0
+    while parent < n:
+        if parent * 2 + 1 < n and array[array[parent]] > array[array[parent * 2 + 1]]:
+            minimum = parent * 2 + 1
+        if parent * 2 + 2 < n and array[array[parent]] > array[array[parent * 2 + 2]]:
+            minimum = parent * 2 + 2
+        if parent != minimum:
+            array[parent], array[minimum] = array[minimum], array[parent]
+            parent = minimum
+        else:
+            break
+    return array
+
+
+def assignCanonical(frequencies, lengths):
+    n = len(lengths)
+    maxlength = max(lengths)
+    nums = [0] * maxlength
+    print(nums, lengths)
+    for length in lengths:
+        nums[length - 1] += 1
+    print(nums)
+
+
 def main():
     message = input()
-    frequencies = readFrequency(message)
-    nodes = create(frequencies)
-    root = buildTree(nodes)
-    values = assignCodes(root)
-    newMessage = writeCode(message, values)
-    # print(f'values: {values}')
-    print(f'old message: {"".join(bin(ord(c)) for c in message).replace("b","")}')
-    print(f'new message: {newMessage}')
-    # print(f'message decoded is: {decode(newMessage, root)}')
-    # print(bfs(root))
-    calculateLength(frequencies)
+    frequencies = readFrequencies(message)
+    lenghts = calculateLength(frequencies)
+    assignCanonical(frequencies, lenghts)
 
 
 main()
